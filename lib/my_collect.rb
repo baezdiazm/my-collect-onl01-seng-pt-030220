bde2ee3bbbdc2682f8567a05c6a41626d1257f79
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-03-16 04:52:04 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,14 +1,14 @@
 #
 
-# def my_collect(students)
-#   i=0
-#   newarray = []
-#   while i< students.length do
-#     newarray << (students[i].split(" ").first)
-#     i +=1
-#   end
-# return newarray
-# end
+def my_collect(students)
+  i=0
+  newarray = []
+  while i< students.length do
+    newarray << (students[i].split(" ").first)
+    i +=1
+  end
+return newarray
+end
 
 def my_collect(languages)
   i=0
