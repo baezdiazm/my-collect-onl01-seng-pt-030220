c0a726067f71f858c7e2460b91c677660439c9a2
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-03-16 04:15:15 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,11 +3,9 @@ names = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 def my_collect(names)
   i=0
   newarray = []
-  another = []
   while i< names.length do
     newarray << (names[i].split(" ").first)
     i +=1
   end
   return newarray
 end
-my_collect(names)
