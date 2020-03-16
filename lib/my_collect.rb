bf0edaac816dfda92ac7944078712df5f46db6b6
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-03-16 04:55:59 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -13,8 +13,8 @@ end
 def my_collect(languages)
   i=0
   upcased = []
-  while i< languages.length do |language|
-    upcased << languages[i].upcase
+  while i< languages.length do
+    {|language|   upcased << languages[i].upcase}
     i +=1
   end
 return upcased
