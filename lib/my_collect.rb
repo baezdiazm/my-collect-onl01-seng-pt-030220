cb9b24c1370af1c6318502cad37706dd2a88f00f
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-03-16 04:53:18 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -14,7 +14,7 @@ def my_collect(languages)
   i=0
   upcased = []
   while i< languages.length do
-    upcased << languages[i].split(" ").first.upcase.downcase
+    upcased << languages[i].split(" ").first
     i +=1
   end
 return upcased
