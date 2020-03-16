3143e6118d97d25da271a7b964d459484d48e4d7
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-03-16 04:27:32 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,21 +1,21 @@
 # names = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
-def my_collect(students)
-  i=0
-  newarray = []
-  while i< students.length do
-    newarray << (students[i].split(" ").first)
-    i +=1
-  end
-return newarray
-end
-
-# def my_collect(languages)
+# def my_collect(students)
 #   i=0
-#   upcased = []
-#   while i< languages.length do
-#     upcased << languages[i].split(" ").first.upcase
+#   newarray = []
+#   while i< students.length do
+#     newarray << (students[i].split(" ").first)
 #     i +=1
 #   end
-# return upcased
+# return newarray
 # end
+
+def my_collect(languages)
+  i=0
+  upcased = []
+  while i< languages.length do
+    upcased << languages[i].split(" ").first.upcase
+    i +=1
+  end
+return upcased
+end
